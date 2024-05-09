cd77a1c0cef9d72b34f6ca081bc320413171c285
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-05-09 11:55:40 +0200

    Initial commit

---
+++ src/test/java/ua/project/googleauthentication/GoogleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.project.googleauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
